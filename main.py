939d807a41ca8b5f7ad8b8d9284c4163ce2c7335
emerson <1514088@senati.pe>
2024-04-29 11:12:32 -0500

    se agrego la calculadora

--- main.py
+++ main.py
@@ -3,5 +3,16 @@ b = 10
 print("hola mundo")
 print("la suma es:", a+b)
 print("la resta es:", a-b)
+print("*********************************")
+print("***************SE AGREGÓ UNA CALCULADORA******************")
+e = int (input("ingre el primer numero:"))
+n = int (input("ingre el segundo numero:"))
+print("la suma es :", e+n)
+print("la resta es :", e-n)
+print("la multiplicacion es :", e*n)
+print("la division es :", e/n)
+print("*********************************")
+print("*********************************")
+print("*********************************")
 
 
